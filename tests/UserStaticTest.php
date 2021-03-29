9ab59d7f78c3bda851b6ec86e8045f89773dad86
miguelmargar <miguelmargar@outlook.com>
2021-03-29 15:45:55 +0100

    added mocking example of static method to test by using Mockery

--- tests/UserStaticTest.php
+++ tests/UserStaticTest.php
@@ -3,11 +3,18 @@
 
     class UserStaticTest extends TestCase {
 
+        public function tearDown(): void {
+            Mockery::close();
+        }
+
         public function testNotifyReturnsTrue() {
             $user = new UserStatic('dave@example.com');
 
             $mailer = $this->createMock(Mailer::class);
 
+            // This needs to be an anonymous function and can not be
+            // the callable [Mailer::class, 'send'] as using this callable
+            // will use the static class directly and not the mock below.
             $user->setMailerCallable(function() {
 
                 echo "mocked";
@@ -16,5 +23,18 @@
             });
 
             $this->assertTrue($user->notify('Hello'));
+        }
+
+        public function testNotifyReturnsTrueWithMockery() {
+            $user = new User('dave@example.com');
+
+            $mock = Mockery::mock('alias:Mailer');
+
+            $mock->shouldReceive('send')
+                 ->once()
+                 ->with($user->email, 'Hello')
+                 ->andReturn(true);
+
+            $this->assertTrue($user->notify('Hello!'));
         }
     }
