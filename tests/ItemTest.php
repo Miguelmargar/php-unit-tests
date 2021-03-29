5537e3a9a15d37e3456900689091c065230d8ad5
miguelmargar <miguelmargar@outlook.com>
2021-03-29 13:55:55 +0100

    added the use of reflector example to test a private method in a class

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -8,15 +8,29 @@
             $this->assertNotEmpty($item->getDescription());
         }
 
+        // Testing a protected method in the class uses the child
+        // class which in turn extends the parent class and used
+        // the parent's method with public visibility
         public function testIdIsAnInteger() {
             $item = new ItemChild;
 
             $this->assertIsInt($item->getID());
         }
 
+        // To test private methods from a class need to use reflectors
+        // as private methods are not inherited by child classes
         public function testTokenIsAString() {
-            $item = new ItemChild;
+            $item = new Item;
 
-            $this->assertIsString($item->getToken());
+            // Create reflector with class that we need
+            $reflector = new ReflectionClass(Item::class);
+            // Get the method that we need to test
+            $method = $reflector->getMethod('getToken');
+            // Set this method to be accessible
+            $method->setAccessible(true);
+            // This is the result
+            $result = $method->invoke($item);
+            // Pass this result to the assert statement
+            $this->assertIsString($result);
         }
     }
